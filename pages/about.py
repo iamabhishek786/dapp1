49fcb7e23f602f1a5669faa71fadc1eab22214a0
Abhishek <abhishekgupta974@yahoo.com>
2023-07-09 11:58:04 +0530

    streamlit basics

--- pages/about.py
+++ pages/about.py
@@ -14,4 +14,4 @@ This app performs simple data analysis on the ***House Price*** dataset.
     - seaborn
 * **Data source:** [Kaggle](https://www.kaggle.com/datasets/amarnadhreddyavuduri/kc-house-dataset)
 <img src= "https://img.freepik.com/free-photo/wide-angle-shot-single-tree-growing-clouded-sky-during-sunset-surrounded-by-grass_181624-22807.jpg?w=1000">
-""", unsafe_allow_html=True)
+""", unsafe_allow_html=True) # unsafe_allow_html allows HTML files.
